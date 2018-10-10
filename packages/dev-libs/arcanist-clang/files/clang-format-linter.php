8f220bb2e1b7866fe9d7f269459da5f647442c71
Mikael Voss <mvs@nyantec.com>
2018-10-10 20:03:41 +0200

    arcanist-clang: Add SCM version

---
+++ packages/dev-libs/arcanist-clang/files/clang-format-linter.php
@@ -0,0 +1,6 @@
+<?php
+arcanist_load_libraries(
+	["/opt/phacility/arcanist/externals/includes/clang-format-linter/"],
+	$must_load = false, $lib_source = "clang-format-linter",
+	ArcanistWorkingCopyIdentity::newDummyWorkingCopy());
+?>
