dfb3c041d2d4743819520c6d2ed06369a3a65914
Mikael Voss <mvs@nyantec.com>
2021-01-17 11:57:40 +0100

    Drop arcanist-clang

--- packages/dev-libs/arcanist-clang/files/clang-format-linter.php
+++
@@ -1,6 +0,0 @@
-<?php
-arcanist_load_libraries(
-	["/opt/phacility/arcanist/externals/includes/clang-format-linter/"],
-	$must_load = false, $lib_source = "clang-format-linter",
-	ArcanistWorkingCopyIdentity::newDummyWorkingCopy());
-?>
